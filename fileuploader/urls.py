a956b8f175245cca79cfd05276a329284f11a549
Aghaei-Amirmasoud <47460170+Aghaei-Amirmasoud@users.noreply.github.com>
2019-02-12 21:37:58 +0330

    Initial commit

---
+++ fileuploader/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from shalgham import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('upload/editor', views.upload, name='upload'),
+    path('upload/editor/gray/', views.gray, name='gray'),
+    path('upload/editor/crop/', views.crop, name='crop'),
+    path('upload/editor/resize/', views.resize, name='resize'),
+    path('upload/editor/rotate/', views.rotate, name='rotate'),
+    path('upload/editor/remove/', views.remove, name='remove'),
+    path('upload/editor/share/', views.share_2, name='share_2'),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.SHARE_URL, document_root=settings.SHARE_ROOT)
---
+++ fileuploader/views.py
@@ -0,0 +1,84 @@
+from random import randint
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.core.files.storage import default_storage
+from PIL import Image
+from shalgham import settings
+import glob, os
+import pathlib
+
+
+
+def index(request):
+    return render(request, 'fileuploader/index.html', {
+        'lst': [1, 2, 3, 4, 5, 'salam'],
+    })
+
+
+def upload(request):
+    try :
+        photo_url = default_storage.save('file.jpg', request.FILES['fileToUpload'])
+        context = {'image' : photo_url}
+        request.session['Url'] = photo_url;
+        return render(request, 'fileuploader/edit.html', context)
+    except :
+        return HttpResponse('No photo chosen')
+
+
+def gray(request):
+    try :
+        img = Image.open(os.path.join(settings.MEDIA_ROOT, request.session['Url']))
+        img_grayed= img.convert("L")
+        img_grayed.save(os.path.join(settings.MEDIA_ROOT,  request.session['Url']))
+        context = {'image' : request.session['Url']}
+        return render(request, 'fileuploader/edit.html', context)
+    except :
+        return HttpResponse('No photo uploaded or photo removed')
+
+
+def crop(request):
+    try :
+        img = Image.open(os.path.join(settings.MEDIA_ROOT,  request.session['Url']))
+        img_croped = img.crop((int(request.POST.get('box1')), int(request.POST.get('box2')), int(request.POST.get('box3')), int(request.POST.get('box4'))))
+        img_croped.save(os.path.join(settings.MEDIA_ROOT, request.session['Url']))
+        context = {'image' : request.session['Url']}
+        return render(request, 'fileuploader/edit.html', context)
+    except :
+        return HttpResponse('No photo uploaded or photo removed')
+
+
+def resize(request):
+    try :
+        img = Image.open(os.path.join(settings.MEDIA_ROOT, request.session['Url']))
+        img_resized = img.resize((int(request.POST.get('rebox1')), int(request.POST.get('rebox2'))))
+        img_resized.save(os.path.join(settings.MEDIA_ROOT,  request.session['Url']))
+        context = {'image' : request.session['Url']}
+        return render(request, 'fileuploader/edit.html', context)
+    except :
+        return HttpResponse('No photo uploaded or photo removed')
+
+def rotate(request):
+    try :
+        img = Image.open(os.path.join(settings.MEDIA_ROOT,  request.session['Url']))
+        img_rotated = img.rotate(int(request.POST.get('rtbox1')), expand=1)
+        img_rotated.save(os.path.join(settings.MEDIA_ROOT ,  request.session['Url']))
+        context = {'image' : request.session['Url']}
+        return render(request, 'fileuploader/edit.html', context)
+    except :
+        return HttpResponse('No photo uploaded or photo removed')
+
+def remove(request):
+    return redirect('/')
+
+def share_2(request):
+    img = Image.open(os.path.join(settings.MEDIA_ROOT, request.session['Url']))
+    img.save(os.path.join(settings.SHARE_ROOT, request.session['Url']))
+    path = os.path.join(settings.SHARE_ROOT)
+    os.chdir(path)
+    files = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
+    lst_image = []
+    for file in range(len(files)-1,-1,-1):
+        lst_image.append(files[file])
+    return render(request, 'fileuploader/sharepanel.html', {
+        'lst_image': lst_image,
+        })
